5daa008bd96655f6cf18b42b46b06f1678f85a09
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-02-04 20:53:28 +0100

    removed newline from segfault

--- tests/get_next_line/gnlTester/utils/sigsegv.cpp
+++ tests/get_next_line/gnlTester/utils/sigsegv.cpp
@@ -6,13 +6,13 @@ extern int iTest;
 void sigsegv(int signal)
 {
 	(void)signal;
-	cout << FG_LYELLOW << iTest++ << ".SIGSEGV" << ENDL;
+	cout << FG_LYELLOW << iTest++ << ".SIGSEGV";
 	exit(EXIT_SUCCESS);
 }
 
 void sigalarm(int signal)
 {
 	(void)signal;
-	cout << FG_LYELLOW << iTest++ << ".TIMEOUT" << ENDL;
+	cout << FG_LYELLOW << iTest++ << ".TIMEOUT";
 	exit(EXIT_SUCCESS);
 }
